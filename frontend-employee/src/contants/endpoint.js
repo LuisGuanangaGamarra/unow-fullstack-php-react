c0059fb8396e84943fcc4d9ee2ce9ce27fcceb97
Luis Guananga <luisgamarra_97@hotmail.com>
2025-02-14 04:51:42 -0500

    add frontend prokect

--- frontend-employee/src/contants/endpoint.js
+++ frontend-employee/src/contants/endpoint.js
@@ -3,7 +3,7 @@ const ENDPOINTS = {
     REGISTER: 'http://localhost:8000/api/users/register',
     EMPLOYEES: 'http://localhost:8002/api/employees',
     EMPLOYEES_DELETE_OR_UPDATE: (id) => `http://localhost:8002/api/employees/${id}`,
-    POSITIONS_API: 'https://ibillboard.com/api/positions',
+    POSITIONS_API: '/puesto/api/positions',
 };
 
 export default ENDPOINTS;
